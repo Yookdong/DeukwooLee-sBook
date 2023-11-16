17b96f5a7f9bf9ae28b595c262624664fb457959
Yookdong <ydhydh45@gmail.com>
2023-11-16 16:12:53 +0900

    create project

---
+++ Source/DeukwooLee/DeukwooLeeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DeukwooLeeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEUKWOOLEE_API ADeukwooLeeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
